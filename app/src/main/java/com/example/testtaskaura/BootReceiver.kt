ea4fa7e48ce2d796330777b2cd4ce27a2b2d04f2
roman.pozdnyakov <roman.pozdniakov@itexus.com>
2024-08-03 10:45:08 +0200

    listen to the BOOT_EVENT, create Broadcast Receiver

---
+++ app/src/main/java/com/example/testtaskaura/BootReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.testtaskaura
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d("BootReceiver", "Device booted")
+        }
+    }
+}
